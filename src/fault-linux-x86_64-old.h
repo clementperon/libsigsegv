52591612cb6392ad5a368d000549becfa90de9d0
Clément Péron <peron.clem@gmail.com>
2025-02-08 19:37:46 +0100

    hack

--- src/fault-linux-x86_64-old.h
+++ src/fault-linux-x86_64-old.h
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-#include <asm/sigcontext.h>
+#include <signal.h>
 
 #define SIGSEGV_FAULT_HANDLER_ARGLIST  int sig, struct sigcontext sc
 #define SIGSEGV_FAULT_CONTEXT  (&sc)
